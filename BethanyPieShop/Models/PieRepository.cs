61458108cba3396f05efe1a40c6b67b6409bb015
mnsimbam <michel.nsimbamatondo@student.unamur.be>
2022-07-07 19:50:41 +0200

    Adding PieRepository

---
+++ BethanyPieShop/Models/PieRepository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BethanyPieShop.Models
+{
+    public class PieRepository : IPieRepository
+    {
+        private readonly BethanysPieShopDbContext _bethanysPieShopDbContext;
+
+        public PieRepository(BethanysPieShopDbContext bethanysPieShopDbContext)
+        {
+            _bethanysPieShopDbContext = bethanysPieShopDbContext;
+        }
+
+        public IEnumerable<Pie> AllPies
+        {
+            get
+            {
+                return _bethanysPieShopDbContext.Pies.Include(p => p.Category);
+            }
+        }
+
+        public IEnumerable<Pie> PiesOfTheWeek
+        {
+            get
+            {
+                return _bethanysPieShopDbContext.Pies.Include(p => p.Category).Where(p => p.IsPieOfTheWeek);
+            }
+        }
+        public Pie? GetPieById(int pieId)
+        {
+            return _bethanysPieShopDbContext.Pies.FirstOrDefault(p => p.PieId == pieId);
+        }
+
+        public IEnumerable<Pie> SearchPies(string searchQuery)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
