43fefb944c9d653c0b07c74ddd624cd81d2e38da
mnsimbam <michel.nsimbamatondo@student.unamur.be>
2022-07-07 19:01:09 +0200

    Configuring the application ConnectionStrings to use Entity Framework Core 6

--- BethanyPieShop/Program.cs
+++ BethanyPieShop/Program.cs
@@ -1,9 +1,16 @@
 using BethanyPieShop.Models;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
 builder.Services.AddScoped<IPieRepository, MockPieRepository>();
+
+builder.Services.AddDbContext<BethanysPieShopDbContext>(opts =>
+{
+    opts.UseSqlServer(builder.Configuration["ConnectionStrings:BethanysPieShopDbCtxConnection"]);
+    opts.EnableSensitiveDataLogging(true);
+});
 
 builder.Services.AddControllersWithViews();
 
