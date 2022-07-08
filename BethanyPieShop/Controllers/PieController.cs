99673726bc48e0fb9c10da4e8bfc1dda5f32bf4c
mnsimbam <michel.nsimbamatondo@student.unamur.be>
2022-07-08 15:53:43 +0200

    Adding Details method and view for a Pie

--- BethanyPieShop/Controllers/PieController.cs
+++ BethanyPieShop/Controllers/PieController.cs
@@ -23,5 +23,14 @@ namespace BethanyPieShop.Controllers
             PieListViewModel pieListViewModel = new PieListViewModel(_pieRepository.AllPies, "Cheese cakes");
             return View(pieListViewModel);
         }
+
+        public IActionResult Details(int id)
+        {
+            var pie = _pieRepository.GetPieById(id);
+
+            if (pie == null)
+                return NotFound();
+            return View(pie);
+        }
     }
 }
