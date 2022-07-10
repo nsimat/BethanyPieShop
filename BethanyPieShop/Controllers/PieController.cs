fc6d9d6a21dadacfba1153a5c087952e9e807312
mnsimbam <michel.nsimbamatondo@student.unamur.be>
2022-07-10 20:48:49 +0200

    Adding CategoryMenu to display pies according their menus

--- BethanyPieShop/Controllers/PieController.cs
+++ BethanyPieShop/Controllers/PieController.cs
@@ -16,13 +16,34 @@ namespace BethanyPieShop.Controllers
             _categoryRepository = categoryRepository;
         }
 
-        public IActionResult List()
+        //public IActionResult List()
+        //{
+        //    // ViewBag.CurrentCategory = "Cheese cakes";
+        //    // return View(_pieRepository.AllPies);
+        //    PieListViewModel pieListViewModel =
+        //        new PieListViewModel(_pieRepository.AllPies, "All our pies");
+        //    return View(pieListViewModel);
+        //}
+
+        public IActionResult List(string category)
         {
-            // ViewBag.CurrentCategory = "Cheese cakes";
-            // return View(_pieRepository.AllPies);
-            PieListViewModel pieListViewModel =
-                new PieListViewModel(_pieRepository.AllPies, "All our pies");
-            return View(pieListViewModel);
+            IEnumerable<Pie> pies;
+            string? currentCategory;
+
+            if (string.IsNullOrEmpty(category))
+            {
+                pies = _pieRepository.AllPies.OrderBy(p => p.PieId);
+                currentCategory = "All pies";
+            }
+            else
+            {
+                pies = _pieRepository.AllPies.Where(p => p.Category.CategoryName == category).OrderBy(p => p.PieId);
+                currentCategory = _categoryRepository.AllCategories
+                    .FirstOrDefault(c => c.CategoryName == category)
+                    ?.CategoryName;
+            }
+
+            return View(new PieListViewModel(pies, currentCategory));
         }
 
         public IActionResult Details(int id)
