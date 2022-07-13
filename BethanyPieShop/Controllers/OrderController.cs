58a879a12b832a0311bed97f1cecabf8dfcb5d39
mnsimbam <michel.nsimbamatondo@student.unamur.be>
2022-07-13 23:11:36 +0200

    Adding the Checkout process for an order

---
+++ BethanyPieShop/Controllers/OrderController.cs
@@ -0,0 +1,22 @@
+﻿using BethanyPieShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanyPieShop.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IShoppingCart _shoppingCart;
+
+        public OrderController(IOrderRepository orderRepository, IShoppingCart shoppingCart)
+        {
+            _orderRepository = orderRepository;
+            _shoppingCart = shoppingCart;
+        }
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+    }
+}
