d2b2fb5a96701897d483efdf7ccdd15f0e9f4aa5
mnsimbam <michel.nsimbamatondo@student.unamur.be>
2022-07-17 13:54:32 +0200

    Adding test for PieController

---
+++ BethanyPieShopTests/Controllers/PieControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using BethanyPieShop.Controllers;
+using BethanyPieShop.ViewModels;
+using BethanyPieShopTests.Mocks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanyPieShopTests.Controllers
+{
+    public class PieControllerTests
+    {
+        [Fact]
+        public void List_EmptyCategory_ReturnAllPies()
+        {
+            //arrange
+            var mockPieRepository = RepositoryMocks.GetPieRepository();
+            var mockCategoryRepository = RepositoryMocks.GetCategoryRepository();
+
+            var pieController = new PieController(mockPieRepository.Object, mockCategoryRepository.Object);
+
+            //act
+            var result = pieController.List("");
+
+            //assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var pieListViewModel = Assert.IsAssignableFrom<PieListViewModel>(viewResult.ViewData.Model);
+            Assert.Equal(10, pieListViewModel.Pies.Count());
+        }
+    }
+}
