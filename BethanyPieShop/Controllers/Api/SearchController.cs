667ee3f2a71ce74f8b655a033168f7c938a05258
mnsimbam <michel.nsimbamatondo@student.unamur.be>
2022-07-22 21:38:43 +0200

    Adding the SearchController for REST API

---
+++ BethanyPieShop/Controllers/Api/SearchController.cs
@@ -0,0 +1,46 @@
+﻿using BethanyPieShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanyPieShop.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+        private readonly IPieRepository _pieRepository;
+
+        public SearchController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllPies()
+        {
+            var allPies = _pieRepository.AllPies;
+            return Ok(allPies);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetPieById(int id)
+        {
+            if (!_pieRepository.AllPies.Any(p => p.PieId == id))
+                return NotFound();
+
+            return Ok(_pieRepository.AllPies.Where(p => p.PieId == id));
+        }
+
+        [HttpPost]
+        public IActionResult SearchPies([FromBody] string searchQuery)
+        {
+            IEnumerable<Pie> pies = new List<Pie>();
+
+            if (!string.IsNullOrEmpty(searchQuery))
+            {
+                pies = _pieRepository.SearchPies(searchQuery);
+            }
+
+            return new JsonResult(pies);
+        }
+    }
+}
