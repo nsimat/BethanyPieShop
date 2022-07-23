f361d32a5b71ad1a28d3d2404e9db85a4f977804
mnsimbam <michel.nsimbamatondo@student.unamur.be>
2022-07-23 23:32:02 +0200

    Adding site interaction with Blazor

---
+++ BethanyPieShop/Pages/App/PieCard.razor.cs
@@ -0,0 +1,11 @@
+﻿using BethanyPieShop.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace BethanyPieShop.Pages.App
+{
+    public partial class PieCard
+    {
+        [Parameter]
+        public Pie? Pie { get; set; }
+    }
+}
